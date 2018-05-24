2293a2fbeab3819a68a7b29a88fddf16306239aa
sjkyspa <sjkyspa@gmail.com>
2018-05-24 19:11:27 +0800

    should able to truffle to development

--- truffle.js
+++ truffle.js
@@ -2,9 +2,15 @@ const TestRPC = require("ganache-cli");
 
 module.exports = {
     networks: {
-        development: {
+        test: {
             provider: TestRPC.provider({total_accounts: 10}),
             network_id: "*"
+        },
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*", // Match any network id
+            gas: 4612388
         }
     }
 };
