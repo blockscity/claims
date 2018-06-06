f8be716e47ff445cd4f03cd5309e4f6992d87725
sjkyspa <sjkyspa@gmail.com>
2018-06-06 12:50:07 +0800

    should able to handle cornner case

--- src/claims.js
+++ src/claims.js
@@ -1,9 +1,14 @@
 import Web3 from 'web3';
-
 export const contract = (client, address) => {
+    if (!Web3.utils.isAddress(address)) {
+        throw new Error(`contract address ${address} invalid`);
+    }
     return new Claims(client, address);
 };
 
+const bytes2s = (key) => {
+    return Buffer.from(key.split('00').join('').slice(2), 'hex').toString();
+};
 
 class Claims {
     constructor(client, address) {
@@ -28,6 +33,14 @@ class Claims {
     }
 
     async take(key) {
+        if (this.issuer === undefined || this.issuer === null) {
+            return Promise.reject("issuer needed")
+        }
+
+        if (this.subject === undefined || this.subject === null) {
+            return Promise.reject("subject needed")
+        }
+
         let hash = await this.client.call({
             to: this.address,
             data: {
@@ -41,7 +54,12 @@ class Claims {
             }
         });
 
-        return await this._sidecar.take(hash);
+        let v = bytes2s(hash);
+        if (v === "") {
+            return "";
+        }
+
+        return await this._sidecar.take(v);
     }
 
     place(key, value) {
--- src/claims.spec.js
+++ src/claims.spec.js
@@ -4,9 +4,21 @@ const expect = chai.expect;
 import {contract} from './claims';
 import {IPFS} from "./ipfs";
 import {ChainClient} from './chainclient';
+import Web3 from "web3";
+import ganache from "ganache-cli";
+import Claims from '../build/contracts/Claims.json';
 
 
 describe('claims', () => {
+    let web3;
+    let chainClient;
+    let claims;
+    let issuer;
+    let subject;
+    let pseudo;
+    let key;
+    let unknownKey;
+    let value;
     const identity =
         `{
             "@context": "https://w3id.org/did/v1",
@@ -23,15 +35,35 @@ describe('claims', () => {
             }]
         }`;
 
-    let client = new ChainClient();
 
     let ipfs = new IPFS();
 
 
     before(async () => {
-        client.call = (tx) => {
-            return "ipfs_hash_for_kayla"
-        };
+        web3 = new Web3();
+        web3.setProvider(ganache.provider());
+        let accounts = await web3.eth.getAccounts();
+        issuer = accounts[0];
+        subject = accounts[1];
+        pseudo = accounts[2];
+        key = "BlockCityPassport";
+        value = "ipfs_hash_for_kayla";
+        unknownKey = "unknownKey";
+
+        let deployed = await new web3.eth.Contract(Claims.abi)
+            .deploy({data: Claims.bytecode, arguments: []})
+            .send({
+                from: issuer,
+                gas: 1500000,
+                gasPrice: '30000000000000'
+            });
+        claims = deployed.options.address;
+
+        await deployed.methods.set(subject, web3.utils.toHex(key), web3.utils.toHex(value)).send({
+            from: issuer
+        });
+
+        chainClient = new ChainClient(web3);
         ipfs.take = (id) => {
             if (id === "ipfs_hash_for_kayla") {
                 return identity;
@@ -42,11 +74,106 @@ describe('claims', () => {
     });
 
     it('should able to get the claim issued by issuer for subject', async () => {
-        let value = await contract(client, "0x0000")
+        let value = await contract(chainClient, claims)
             .sidecar(ipfs)
-            .from("0xissuer")
-            .for("0xsubject")
-            .take("BlockCityPassport");
+            .from(issuer)
+            .for(subject)
+            .take(key);
         expect(value).to.be.equal(identity);
+    });
+
+    it('should not able to get the claim with non existed issuer', async () => {
+        let value = await contract(chainClient, claims)
+            .sidecar(ipfs)
+            .from(pseudo)
+            .for(subject)
+            .take(key);
+        expect(value).to.be.equal("");
+    });
+
+    it('should not able to get the claim with non existed subject', async () => {
+        let value = await contract(chainClient, claims)
+            .sidecar(ipfs)
+            .from(issuer)
+            .for(pseudo)
+            .take(key);
+        expect(value).to.be.equal("");
+    });
+
+    it('should not able to get the empty claim when key not set', async () => {
+        let value = await contract(chainClient, claims)
+            .sidecar(ipfs)
+            .from(issuer)
+            .for(subject)
+            .take(unknownKey);
+        expect(value).to.be.equal("");
+    });
+
+    it('should error when no issuer defined', async () => {
+        await contract(chainClient, claims)
+            .sidecar(ipfs)
+            .for(subject)
+            .take(key)
+            .then(() => {
+                return Promise.reject("should remind set the issuer");
+            })
+            .catch(e => {
+                if (typeof e === "string" && e === "issuer needed") {
+                    // ok
+                } else {
+                    expect.fail("should remind set the issuer")
+                }
+            });
+    });
+
+    it('should error when no subject defined', async () => {
+        await contract(chainClient, claims)
+            .sidecar(ipfs)
+            .from(issuer)
+            .take(key)
+            .then(() => {
+                return Promise.reject("should remind set the subject");
+            })
+            .catch(e => {
+                if (typeof e === "string" && e === "subject needed") {
+                    // ok
+                } else {
+                    throw new Error("should remind set the subject");
+                }
+            });
+    });
+
+    it('should error when no subject defined', async () => {
+        await contract(chainClient, claims)
+            .sidecar(ipfs)
+            .from(issuer)
+            .take(key)
+            .then(() => {
+                return Promise.reject("should remind set the subject");
+            })
+            .catch(e => {
+                if (typeof e === "string" && e === "subject needed") {
+                    // ok
+                } else {
+                    throw new Error("should remind set the subject");
+                }
+            });
+    });
+
+    it('should error claims not valid address', async () => {
+        let invalidAddress = "0x000000";
+        try {
+            await contract(chainClient, invalidAddress)
+                .sidecar(ipfs)
+                .from(issuer)
+                .for(subject)
+                .take(key);
+        } catch (e) {
+            if (typeof e === "object" && e.message === `contract address ${invalidAddress} invalid`) {
+                // ok
+            } else {
+                throw new Error("init contract with valid address");
+            }
+        }
     });
 });
